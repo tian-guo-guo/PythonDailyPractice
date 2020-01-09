b9e2bbd07e8c67a8e8d67bc0afb74f71731d0279
t <tianguoguo15@163.com>
2020-01-09 13:43:58 +0800

    modified some files

--- 数据结构与算法 python语言描述/第三章 线性表/LCList.py
+++ 数据结构与算法 python语言描述/第三章 线性表/LCList.py
@@ -8,7 +8,12 @@ class LinkedListUnderflow(ValueError):
 
 class LCList:       # 循环单链表类
     def __init__(self):
+        # 单链表的变形循环单链表，最后一个结点的next域不用None，而是指向表的第一个结点
+        # 这样可以同时支持O(1)时间的表头/表尾插入 和 O(1)时间的表头删除。
+        # 循环单链表操作和普通单链表的差异就在于扫描循环的结束控制。
         # 这种表对象只需一个数据域_rear，它在逻辑上始终引用着表的尾结点。
+        # 前端加入结点，就是在尾结点和首结点之间加入新的首结点，尾结点引用不变，
+        # 尾端加入新结点，需要更新尾结点引用。
         self._rear = None
     def is_empty(self):
         return self._rear is None 
