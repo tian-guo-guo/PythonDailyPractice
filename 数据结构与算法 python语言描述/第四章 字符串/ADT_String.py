b40b53bc45b537becbfdbe8bbe3c24e1186115f9
t <tianguoguo15@163.com>
2020-01-11 08:51:38 +0800

    add datastruct4 string files

---
+++ 数据结构与算法 python语言描述/第四章 字符串/ADT_String.py
@@ -0,0 +1,11 @@
+'''
+ADT String: 
+    String(self,sseq)        #基于字符序列sseq建立-一个字符串
+    is_ empty(self)          #判断本字符串是否空串
+    len(self)                #取得字符串的长度
+    char(self,index)         #取得字符串中位置index的字符
+    substr(self,a,b)         #取得字符串中[a:b]的子串，左闭右开区间
+    match(self,string)       #查找串string在本字符串中第- - 个出现的位置
+    concat(self,string)      #做出本字符串与另-字符串string的拼接串
+    subst(self,strl,str2)    #做出将 本字符串里的子串str|都替换为str2的结果串
+'''
